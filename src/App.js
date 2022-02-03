9a6ed58d27b465b181e913f20f88e2a8e85d7d87
LeeJaeHoon <dlwogns3413@naver.com>
2022-02-04 05:52:47 +0900

    feat: clear Store Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main/Main";
+import Store from "./pages/Store/Store";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/store/:storeId" element={<Store />} />
       </Routes>
     </Router>
   );
